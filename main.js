e4f3d7606211e77517889de8d7350d76c8a59b3e
a1barif2h <a1barif2h@gmail.com>
2020-07-22 11:10:08 +0600

    added deposit and withdrow section and

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+//login button add event handeler;
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
-    const transectionArea = document.getElementById("transection-area");
-    transectionArea.style.display = "block";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
 });
+
+//deposit button add event handeler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+    updateSpanTag("currentDeposit", depositNumber);
+    updateSpanTag("currentBalance", depositNumber);
+
+    document.getElementById("depositAmount").value = "";
+});
+
+function updateSpanTag(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = currentNumber + depositNumber;
+    document.getElementById(id).innerText = total;
+}
